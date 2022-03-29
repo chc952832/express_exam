e0aaf76897d7db5c8318267babf931763c81e502
chc952832 <littlekyuelf@gmail.com>
2022-03-30 04:03:27 +0800

    modify code of function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@ const url = window.location.pathname
 const route = url.split('/')[1]  // 確認頁面跳轉之後使用者所處頁面
 
 // 排除點擊「回到首頁」按鈕的狀況
-if (route.length !== 0) {
+if (route.length > 0) {
   const button = document.querySelector(`.${route}`)
   console.log(button)
   button.classList.add('clicked')
